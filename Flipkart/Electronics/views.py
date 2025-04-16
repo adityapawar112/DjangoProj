18f34521cc51459ee2f62c3a3e66fb08da544736
adityapawar112 <adityapawar112358@gmail.com>
2025-04-16 19:07:47 +0530

    Setup home page and products module

--- Flipkart/Electronics/views.py
+++ Flipkart/Electronics/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product  # Import the Product model
 # Create your views here.
+
+#Define the home view
+def home(request): 
+    prod = Product.objects.all() # Fetch all products from the database
+    return render(request, 'Electronics/home.html', {'prod':prod}) # Render the home.html template with the products context
--- Flipkart/Flipkart/urls.py
+++ Flipkart/Flipkart/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Electronics import views  # Import views from the Electronics app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Add this line to include the home view
 ]
