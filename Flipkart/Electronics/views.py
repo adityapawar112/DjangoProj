8f9921a938d52dfffdc81cea583f293226291dbb
adityapawar112 <adityapawar112358@gmail.com>
2025-04-17 16:14:06 +0530

    Form Implemented

--- Flipkart/Electronics/views.py
+++ Flipkart/Electronics/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product  # Import the Product model
+from .forms import ProductForm
+
 # Create your views here.
 
 #Define the home view
 def home(request): 
     prod = Product.objects.all() # Fetch all products from the database
-    return render(request, 'Electronics/home.html', {'prod':prod}) # Render the home.html template with the products context
+    fm = ProductForm() 
+    if request.method == 'POST':
+        fm = ProductForm(request.POST)
+        if fm.is_valid(): # Validate the form data
+            fm.save()
+            return redirect('home')
+    else:
+        fm = ProductForm()
+    return render(request, 'Electronics/home.html', {'prod':prod, 'fm':fm}) # Render the home.html template with the products context
+
