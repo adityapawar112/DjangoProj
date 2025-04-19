d8fb47f276e006fd9ee49cb309ae49c3bbb6fe06
adityapawar112 <adityapawar112358@gmail.com>
2025-04-19 16:22:57 +0530

    Implemented Photos

--- Flipkart/Electronics/views.py
+++ Flipkart/Electronics/views.py
@@ -9,7 +9,7 @@ def home(request):
     prod = Product.objects.all() # Fetch all products from the database
     fm = ProductForm() 
     if request.method == 'POST':
-        fm = ProductForm(request.POST)
+        fm = ProductForm(request.POST, request.FILES)  # Bind the form with POST data and files
         if fm.is_valid(): # Validate the form data
             fm.save()
             return redirect('home')
--- Flipkart/Flipkart/urls.py
+++ Flipkart/Flipkart/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Electronics import views  # Import views from the Electronics app
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Add this line to include the home view
     path('electronics/', include('Electronics.urls')),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
